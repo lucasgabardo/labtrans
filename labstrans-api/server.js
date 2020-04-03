fd4845cbbd23cdcfc46afa74c41617a2d31b7b87
unknown <lucasgabar@gmail.com>
2020-04-03 01:25:35 -0300

    changing files path

--- labstrans-api/server.js
+++ labstrans-api/server.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('app/data/sqlitedb');
+var db = new sqlite3.Database('src/data/sqlitedb');
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, db);
+require('./src/routes')(app, db);
 
 app.listen(port, () => {
     console.log('Backend NodeJS live on ' + port);
